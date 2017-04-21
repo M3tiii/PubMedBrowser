be0e3b1b868476290585ccf865aef46a441397d1
M3tiii <m1995g@gmail.com>
2017-04-21 20:56:37 +0200

    Setup default lucene indexer.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String [] args) {
-
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
         System.out.println("hello");
     }
 }
