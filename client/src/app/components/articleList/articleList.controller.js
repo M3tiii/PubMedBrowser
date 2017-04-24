dc974eb5c3bf7e8d30a6b5585f0f67865ef1b8ae
M3tiii <m1995g@gmail.com>
2017-04-24 16:40:58 +0200

    Added article-list component.

---
+++ client/src/app/components/articleList/articleList.controller.js
@@ -0,0 +1,13 @@
+class ArticleList {
+  constructor() {
+    this.list = [{
+      title: 'title1',
+      score: 0.61
+    }, {
+      title: 'title2',
+      score: 0.39
+    }];
+  }
+}
+
+export default ArticleList;
--- client/src/app/components/index.js
+++ client/src/app/components/index.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 
 import ArticleComponents from './article/article.component';
+import ArticleListComponents from './articleList/articleList.component';
 
 const components = angular.module('PubMedBrowser.component', [])
   .component('article', ArticleComponents)
+  .component('articleList', ArticleListComponents)
   .name;
 
 export default components;
