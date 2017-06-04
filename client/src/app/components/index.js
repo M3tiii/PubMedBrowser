5fd0228e5a2019a4570bb75216ed0cabd3104337
M3tiii <m1995g@gmail.com>
2017-06-05 00:41:38 +0200

    Set view of search page.

--- client/src/app/components/index.js
+++ client/src/app/components/index.js
@@ -3,7 +3,7 @@ import angular from 'angular';
 import ArticleComponents from './article/article.component';
 import ArticleListComponents from './articleList/articleList.component';
 
-const components = angular.module('PubMedBrowser.component', [])
+const components = angular.module('PubMedBrowser.component', ['ui.bootstrap', 'ngAnimate', 'ngTouch', 'ngSanitize'])
   .component('article', ArticleComponents)
   .component('articleList', ArticleListComponents)
   .name;
